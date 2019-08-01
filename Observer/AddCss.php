8571730084a8af2fdd7434354396ec81078428c3
Adele Clark <adeleclark@rezolve.com>
2019-08-01 03:52:42 +0300

    done

---
+++ Observer/AddCss.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Adele\AddCss\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use \Magento\Framework\View\Asset\Repository;
+use Magento\Framework\App\View\Asset\Publisher;
+use Magento\Customer\Model\Session;
+
+
+class AddCss implements ObserverInterface
+{
+    protected $_assetRepo;
+    protected $_publisher;
+    protected $_customerSession;
+
+    public function __construct(Repository $assetRepo,
+                                Publisher $publisher,
+                                Session $_customerSession
+    ) {
+        $this->_customerSession = $_customerSession;
+        $this->_assetRepo = $assetRepo;
+        $this->_publisher = $publisher;
+    }
+
+    /**
+     * @param Observer $observer
+     *
+     * @return void
+     */
+    public function execute(Observer $observer)
+    {
+        if (!$this->_customerSession->isLoggedIn()) {
+            return;
+        }
+        $asset  = $this->_assetRepo->createAsset('css/add-css.css', ['module' => 'Adele_AddCss', 'area' => 'frontend']);
+        $this->_publisher->publish($asset);
+        $url    = $asset->getSourceUrl();
+        $response = $observer->getEvent()->getData('response');
+        $head = '<link  rel="stylesheet" type="text/css"  media="all" href="' . $url . '" />';
+        $response->setBody(preg_replace('/<\/head>/', $head . '</head>', $response->getBody(), 1));
+    }
+}
